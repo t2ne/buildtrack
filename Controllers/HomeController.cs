a8becfe27fa682d13ffd0ec7e021fe39f3c0166b
t2ne <t8n333@gmail.com>
2025-11-04 17:23:42 +0000

    falta fix sidebar

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,32 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using BuildTrackMVC.Models;
+using BuildTrackMVC.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace BuildTrackMVC.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     // Home agora é dashboard
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        ViewBag.ClientesCount = await _context.Clientes.CountAsync();
+        ViewBag.MateriaisCount = await _context.Materiais.CountAsync();
+        ViewBag.ObrasAtivasCount = await _context.Obras.CountAsync(o => o.Ativa);
+        ViewBag.MovimentosCount = await _context.Movimentos.CountAsync();
+        ViewBag.RegistosMaoObraCount = await _context.RegistosMaoObra.CountAsync();
+        ViewBag.RegistosPagamentosCount = await _context.RegistosPagamentos.CountAsync();
+
         return View();
     }
 
